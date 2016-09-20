8a96fa07094471dfba5c11df0a2c04b5a941a4ec
Will Ridgers <will@mindfuzz.net>
2016-09-20 09:45:31 +0100

    Updated ESDocs.

--- esdocs/js/main.js
+++ esdocs/js/main.js
@@ -1,13 +1,4 @@
 
-//= require vendor/angular.js
-//= require vendor/angular-route.js
-//= require vendor/angular-resource.js
-
-//= require controllers/ContentsListCtrl.js
-//= require controllers/SearchCtrl.js
-//= require controllers/ClassCtrl.js
-//= require controllers/ElementCtrl.js
-
 var esDocApp = angular.module('esDocApp', ['ngRoute']).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider.when('/', { templateUrl: './templates/index.html' }).when('/search', { templateUrl: './templates/search.html', controller: SearchCtrl }).when('/:namespace/:className', { templateUrl: './templates/class.html', controller: ClassCtrl }).when('/:namespace/:className/:elementName', { templateUrl: './templates/element.html', controller: ElementCtrl }).otherwise({
     redirectTo: '/'
